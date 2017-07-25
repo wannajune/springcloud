58797e8c326345dfd0e1424023ffdb07051364bc
wannajune <wanjunspace@163.com>
2017-07-25 13:01:15 +0800

    init

--- consumer/src/main/java/snc/as/consumer/ConsumerController.java
+++ consumer/src/main/java/snc/as/consumer/ConsumerController.java
@@ -4,16 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
 	
+	/*
     @Autowired
     RestTemplate restTemplate;
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add() {
         return restTemplate.getForEntity("http://computeService/add?a=10&b=20", String.class).getBody();
     }
+    */
+	
+	@Autowired
+    private ComputeService computeService;
+	
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String add() 
+	{
+		return computeService.addService();
+	}
 
 }
---
+++ consumerFeign/src/main/java/snc/as/consumerFeign/ConsumerController.java
@@ -0,0 +1,18 @@
+package snc.as.consumerFeign;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+	
+	@Autowired
+    ComputeClient computeClient;
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public Integer add() {
+        return computeClient.add(10, 20);
+    }
+
+}
