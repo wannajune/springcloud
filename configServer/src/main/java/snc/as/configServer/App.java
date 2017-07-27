4dafdacbc918eee0790a5b7a971d33454051d31e
wannajune <wanjunspace@163.com>
2017-07-27 13:13:12 +0800

    init

---
+++ configServer/src/main/java/snc/as/configServer/App.java
@@ -0,0 +1,19 @@
+package snc.as.configServer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+		new SpringApplicationBuilder(App.class).web(true).run(args);
+    }
+}
